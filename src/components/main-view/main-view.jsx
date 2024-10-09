732c6b80ba5ec06f4b8bc5f9a6cb568978ec53a0
sophiefau <sophiefauquembergue@gmail.com>
2024-10-09 19:47:44 +0200

    fetch api and add proptypes

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,44 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      Title: "The Aristocats",
-      image: "https://upload.wikimedia.org/wikipedia/en/8/8d/Aristoposter.jpg",
-      Director: "Wolfgang Reitherman",
-      Cat: "Duchess (mother) and her kittens: Marie, Berlioz, and Toulouse.",
-      Genre: "Family",
-      Year: "1970",
-      Synopsis:
-        "A wealthy old woman plans to leave her fortune to her cats, but her butler tries to get rid of them. The cats must find their way back home with the help of a charming alley cat.",
-    },
-    {
-      id: 2,
-      Title: "The Garfield Movie",
-      image: "https://upload.wikimedia.org/wikipedia/en/9/91/The_Garfield_Movie_2024_poster.jpg",
-      Director: "Peter Hewitt",
-      Cat: "Garfield",
-      Genre: "Animation, Comedy, Family",
-      Year: "2004",
-      Synopsis:
-        "Garfield Garfield, a lazy and sarcastic cat, must deal with a new dog, Odie, in his home. When Odie is kidnapped, Garfield sets out to rescue him.",
-    },
-    {
-      id: 3,
-      Title: "A Street Cat Named Bob",
-      image: "https://upload.wikimedia.org/wikipedia/en/5/51/A_Street_Cat_Named_Bob_%28film%29.png?20170117135509",
-      Cat: "Bob",
-      Genre: "Biography, Drama",
-      Year: "2016",
-      Synopsis:
-        "Based on a true story, the film follows a homeless man struggling with addiction who befriends a stray cat, Bob, and finds a new purpose in life.",
-    },
-  ]);
-
+  const [movies, setMovies] = useState([]);
+    
   const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch("https://catflix-99a985e6fffa.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.docs.map((doc) => {
+          return {
+            id: doc.key,
+            title: doc.title,
+            image: `xxx`,
+            author: doc.author_name?.[0],
+          };
+        });
+
+        setMovies(moviesFromApi);
+      });
+  }, []);
 
   if (selectedMovie) {
     return (
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <div
@@ -5,7 +8,32 @@ export const MovieCard = ({ movie, onMovieClick }) => {
         onMovieClick(movie);
       }}
     >
+      <img
+        src={movie.ImgPath}
+        alt={movie.Title}
+      />
       {movie.Title}
     </div>
   );
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    title: PropTypes.string,
+    Img: PropTypes.string.isRequired,
+    Director: PropTypes.string.isRequired,
+    Cat: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      ColorBreed: PropTypes.string,
+      Bio: PropTypes.string,
+    }).isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired
+    }).isRequired,
+    Year: PropTypes.string.isRequired,
+    Synopsis: PropTypes.string.isRequired,
+    Animation: PropTypes.bool.isRequired,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
 };
