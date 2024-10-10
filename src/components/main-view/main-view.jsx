809e1c0fd526c92bb046b1df19e0ff2bb86b8a9a
sophiefau <sophiefauquembergue@gmail.com>
2024-10-10 16:37:21 +0200

    add signup-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
   const storedUser = JSON.parse(localStorage.getItem("user"));
@@ -36,16 +38,18 @@ export const MainView = () => {
       .catch((error) => console.error('Error fetching movies:', error));
 }, [token]);
 
-  if (!user) {
-    return (
-      <LoginView
-        onLoggedIn={(user, token) => {
-          setUser(user);
-          setToken(token);
-        }}
-      />
-    );
-  }
+if (!user) {
+  return (
+    <>
+      <LoginView onLoggedIn={(user, token) => {
+        setUser(user);
+        setToken(token);
+      }} />
+      or
+      <SignupView />
+    </>
+  );
+}
 
   if (selectedMovie) {
     let similarMovies = movies.filter(
